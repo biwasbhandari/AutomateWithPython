bb5035d11ecd0e036ff587bb75d6ea2c6457bb20
xbiwas <biwas2059@gmail.com>
2024-04-17 19:35:21 +0545

    Chapter3

---
+++ chapter3/chapter3.py
@@ -0,0 +1,19 @@
+def hello():
+    print("Hello")
+    print("Hello World")
+    print("Hello ace")
+
+hello()
+hello()
+hello()
+
+# write a program to add two numbers
+
+a = 1
+b = 1
+
+def addition():
+    add = a + b
+    print(add)
+
+addition()
